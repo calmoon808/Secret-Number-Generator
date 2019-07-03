0f7b9e7a633aead6b5687bc47bcd853c7b6662bc
Calvin <calmoon808@gmail.com>
2019-07-02 21:56:09 -1000

    secret number generator

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,10 @@
 'use strict';
 module.exports = function() {
-  
+    let numbers = [];
+    return function secretNumber(){
+        if (numbers.length === 0){
+            numbers.push(Math.floor(Math.random()) * 1000000);
+        }
+        return numbers;
+    }
 };
